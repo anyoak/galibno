8a6cf74ad9acb09479c53623a20eab970bc34e9d
anyoak <anyoakmail@gmail.com>
2025-11-27 10:54:32 +0300

    Create orangeconfig.py

---
+++ orangeconfig.py
@@ -0,0 +1,19 @@
+# config.py
+
+BOT_TOKEN = "7744467305:AAElMd8DRrtvDcHq9HQvtMTs3G8pO51lsRM"
+
+# Admin Chat ID (যেখানে শুধু ফুল নাম্বার + URL যাবে)
+ADMIN_CHAT_ID = "5878787791"  # Admin user/group ID
+
+# Group Chat ID (যেখানে ভয়েস + OTP যাবে)
+GROUP_CHAT_ID = "-1002360271317"  # Group ID (negative for groups)
+
+# Website Configuration
+LOGIN_URL = "https://www.orangecarrier.com/login"
+CALL_URL = "https://www.orangecarrier.com/live/calls"
+BASE_URL = "https://www.orangecarrier.com"
+
+# System Configuration
+DOWNLOAD_FOLDER = "downloads"
+CHECK_INTERVAL = 3  # seconds
+MAX_ERRORS = 10
