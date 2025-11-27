fc8b15089b039e23fd30def9742078c659325086
anyoak <anyoakmail@gmail.com>
2025-11-27 19:10:37 +0300

    Create royal_config.py

---
+++ royal_config.py
@@ -0,0 +1,19 @@
+# config.py
+
+BOT_TOKEN = "8461039358:AAHAP7WWhDvvqsOnuidSAj4X6l9zXw5G4zM"
+
+# Admin Chat ID (যেখানে শুধু ফুল নাম্বার + URL যাবে)
+ADMIN_CHAT_ID = "6577308099"  # Admin user/group ID
+
+# Group Chat ID (যেখানে ভয়েস + OTP যাবে)
+GROUP_CHAT_ID = "-1002631004312"  # Group ID (negative for groups)
+
+# Website Configuration
+LOGIN_URL = "https://www.royalcall.net/login"
+CALL_URL = "https://www.royalcall.net/live/calls"
+BASE_URL = "https://www.royalcall.net"
+
+# System Configuration
+DOWNLOAD_FOLDER = "downloads"
+CHECK_INTERVAL = 3  # seconds
+MAX_ERRORS = 10
