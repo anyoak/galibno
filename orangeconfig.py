a2057f669b9d658aba24957ac2a6adc522e16de8
anyoak <anyoakmail@gmail.com>
2025-11-28 09:41:13 +0300

    Update orangeconfig.py

--- orangeconfig.py
+++ orangeconfig.py
@@ -1,12 +1,12 @@
 # config.py
 
-BOT_TOKEN = "7744467305:AAElMd8DRrtvDcHq9HQvtMTs3G8pO51lsRM"
+BOT_TOKEN = "8522542742:AAHr7nwP7BBTfOLFBLemRhn4bDe5bySoaIc"
 
 # Admin Chat ID (যেখানে শুধু ফুল নাম্বার + URL যাবে)
-ADMIN_CHAT_ID = "5878787791"  # Admin user/group ID
+ADMIN_CHAT_ID = "6577308099"  # Admin user/group ID
 
 # Group Chat ID (যেখানে ভয়েস + OTP যাবে)
-GROUP_CHAT_ID = "-1002360271317"  # Group ID (negative for groups)
+GROUP_CHAT_ID = "-1002631004312"  # Group ID (negative for groups)
 
 # Website Configuration
 LOGIN_URL = "https://www.orangecarrier.com/login"
