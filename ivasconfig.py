2790da6e404b83c5a8f7b615c29873fd27ac09fd
anyoak <anyoakmail@gmail.com>
2025-11-29 05:55:52 -0700

    Create ivasconfig.py

---
+++ ivasconfig.py
@@ -0,0 +1,10 @@
+# config.py
+BOT_TOKEN = "8328715553:AAEL7XBEQYzxaWSvVYgxwlUb95-hUGW_gQ0"
+CHAT_ID = -1002631004312
+LOGIN_URL = "https://www.ivasms.com/login"
+SMS_URL = "https://www.ivasms.com/portal/live/my_sms"
+BASE_URL = "https://www.ivasms.com/portal"
+DOWNLOAD_FOLDER = "downloads"
+MAX_ERRORS = 5
+CHECK_INTERVAL = 3
+LOGIN_TIMEOUT = 300
